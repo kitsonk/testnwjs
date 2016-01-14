8666c64742bc591c03a887d4790879a73e49ee26
Kitson Kelly <me@kitsonkelly.com>
2016-01-14 13:20:00 +0000

    More goodness

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-export = function main() {
-	return nw;
+export = function main(document: Document, console: Console) {
+	const menu = new nw.Menu();
+
+	menu.append(new nw.MenuItem({
+		label: 'Item A',
+		click() {
+			alert('You have clicked at "Item A"');
+		}
+	}));
+	menu.append(new nw.MenuItem({ label: 'Item B' }));
+	menu.append(new nw.MenuItem({ type: 'separator' }));
+	menu.append(new nw.MenuItem({ label: 'Item C' }));
+
+	document.body.addEventListener('contextmenu', (evt) => {
+		evt.preventDefault();
+		console.log(evt);
+		menu.popup(evt.x, evt.y);
+
+		return false;
+	}, false);
 }
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="nwjs/nwjs-0.13.0.d.ts" />
+/// <reference path="node/node.d.ts" />
